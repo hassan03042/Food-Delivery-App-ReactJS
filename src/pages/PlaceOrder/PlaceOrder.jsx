b8cbb274c7f926e9b7c232bdd82c118819a0f74a
hassan03042 <muhammadhassan6948@gmail.com>
2024-10-17 21:34:18 +0500

    ...

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import './PlaceOrder.css'
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
+        </div>
+        <input type="email" placeholder='Email Address' />
+        <input type="text" placeholder='Street' />
+
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
   )
 }
 
