af7bdc7e25ec75d5d45ed122fcddb4c8f0821a4a
hassan03042 <muhammadhassan6948@gmail.com>
2024-10-18 23:38:47 +0500

    code edited

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext (StoreContext)
+
+  
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -11,9 +16,38 @@ const PlaceOrder = () => {
         </div>
         <input type="email" placeholder='Email Address' />
         <input type="text" placeholder='Street' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone' />
 
       </div>
       <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${5}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 5}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
 
       </div>
     </form>
